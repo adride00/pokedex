5bf314424847b79f2f04d5bb2e7a9ed54d8b4309
adride00 <adride@outlook.com>
2023-03-28 23:32:13 -0600

    se creo la maquetacion de la card

--- src/components/ListPokemons.jsx
+++ src/components/ListPokemons.jsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react'
-import {tipoTierra} from './styles/card.js'
+import {tipoTierra, tipoAgua, font, stats, hover} from './styles/card.js'
 const ListPokemons = () => {
 
   const [pokemons, setPokemons] = useState([])
@@ -15,26 +15,26 @@ const ListPokemons = () => {
             <div className="row g-col-4">
               <div className="col-4 ">
 
-              <div className="card d-flex align-items-center w-5" style={tipoTierra}>
-                <span>N 001</span>
+              <div className="card d-flex align-items-center w-75 rounded" style={tipoAgua}>
+                <span><img src="src/assets/pokebola.png" alt="" width="25" /> N 001</span>
                 <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/shiny/1.png" width='150px' className="p-3 bg-ligth rounded-circle d-flex align-items-center" alt="..." />
                 <div className="card-body w-100">
-                  <h5 className="card-title">Pikachu</h5>
+                  <h5 className="card-title text-center fw-bolder" style={font}>Pikachu</h5>
                   <div className=" text-center">
                     <div className="row gap-2 d-flex justify-content-center">
-                      <div className="col-3 bg-light-subtle rounded-5 border w-25">
-                        <i>icon</i>
-                        <p>Attack</p>
+                      <div className="col-3 bg-light-subtle rounded-3 border w-25">
+                      <i><img src="src/assets/battle.png" width="25" alt="" /></i>
+                        <p className="" style={stats}>Attack</p>
                         <p>124</p>
                       </div>
-                      <div className="col-3 bg-light-subtle rounded-5 border">
-                        <i>icon</i>
-                        <p>Attack</p>
+                      <div className="col-3 bg-light-subtle rounded-3 border">
+                      <i><img src="src/assets/defence.png" width="25" alt="" /></i>
+                        <p className="" style={stats}>Defense</p>
                         <p>124</p>
                       </div>
-                      <div className="col-3 bg-light-subtle rounded-5 border">
-                        <i>icon</i>
-                        <p>Attack</p>
+                      <div className="col-3 bg-light-subtle rounded-3 border">
+                        <i><img src="src/assets/heart-attack.png" width="25" alt="" /></i>
+                        <p className="" style={stats}>Salud</p>
                         <p>124</p>
                       </div>
                 
@@ -42,7 +42,11 @@ const ListPokemons = () => {
                   </div>
                 </div>
                 <div className="card-footer">
-                  <small className="text-body-secondary">Last updated 3 mins ago</small>
+                  <small className="text-body-secondary">
+                  <span class="badge text-bg-primary">Primary</span>
+                  </small>
+                  
+                  
                 </div>
               </div>
 
--- src/components/styles/card.js
+++ src/components/styles/card.js
@@ -1,6 +1,124 @@
 
+
 export const tipoTierra = {
     background: 'linear-gradient(180deg, #f7de3f 50%, #ab9842 50%)',
     backgroundColor: '#f7de3f',
     color: '#212121',
 }
+
+
+export const tipoAgua = {
+    background: 'linear-gradient(180deg, #4592c4 50%, #2d6a8f 50%)',
+    backgroundColor: '#4592c4',
+    color: '#212121',
+}
+
+export const tipoFuego = {
+    background: 'linear-gradient(180deg, #ff4040 50%, #b33939 50%)',
+    backgroundColor: '#ff4040',
+    color: '#212121',
+}
+
+export const tipoPlanta = {
+    background: 'linear-gradient(180deg, #9bcc50 50%, #729a3f 50%)',
+    backgroundColor: '#9bcc50',
+    color: '#212121',
+}
+
+export const tipoElectrico = {
+    background: 'linear-gradient(180deg, #eed535 50%, #a8982b 50%)',
+    backgroundColor: '#eed535',
+    color: '#212121',
+}
+
+export const tipoHielo = {
+    background: 'linear-gradient(180deg, #51c4e7 50%, #3a8fa8 50%)',
+    backgroundColor: '#51c4e7',
+    color: '#212121',
+}
+
+export const tipoPsiquico = {
+    background: 'linear-gradient(180deg, #f366b9 50%, #a93d7f 50%)',
+    backgroundColor: '#f366b9',
+    color: '#212121',
+}
+
+export const tipoBicho = {
+    background: 'linear-gradient(180deg, #729f3f 50%, #4e7329 50%)',
+    backgroundColor: '#729f3f',
+    color: '#212121',
+}
+
+export const tipoVeneno = {
+    background: 'linear-gradient(180deg, #b97fc9 50%, #7b538c 50%)',
+    backgroundColor: '#b97fc9',
+    color: '#212121',
+}
+
+export const tipoFantasma = {
+    background: 'linear-gradient(180deg, #7b62a3 50%, #4e4266 50%)',
+    backgroundColor: '#7b62a3',
+    color: '#212121',
+}
+
+export const tipoDragon = {
+    background: 'linear-gradient(180deg, #53a4cf 50%, #3a6d8a 50%)',
+    backgroundColor: '#53a4cf',
+    color: '#212121',
+}
+
+export const tipoVolador = {
+    background: 'linear-gradient(180deg, #3dc7ef 50%, #269abc 50%)',
+    backgroundColor: '#3dc7ef',
+    color: '#212121',
+}
+
+export const tipoNormal = {
+    background: 'linear-gradient(180deg, #a4acaf 50%, #6d7274 50%)',
+    backgroundColor: '#a4acaf',
+    color: '#212121',
+}
+
+export const tipoLucha = {
+    background: 'linear-gradient(180deg, #d56723 50%, #9f4e1c 50%)',
+    backgroundColor: '#d56723',
+    color: '#212121',
+}
+
+export const tipoRoca = {
+    background: 'linear-gradient(180deg, #a38c21 50%, #6d5c17 50%)',
+    backgroundColor: '#a38c21',
+    color: '#212121',
+}
+
+// estilos para la fuentes
+export const font = {
+    fontFamily: 'Pokemon Solid',
+    fontSize: '1.5rem',
+    fontWeight: 'bold',
+    textShadow: '0 0 5px #000000',
+    color: 'white'
+}
+
+export const stats = {
+    fontSize: '.75rem',
+}
+
+// styles for hover in card
+
+export const hover = {
+    '&:hover': {
+        transform: 'scale(1.1)',
+        transition: 'all 0.5s ease-in-out',
+    }
+}
+
+
+
+
+
+
+
+
+
+
