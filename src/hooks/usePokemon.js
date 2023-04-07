0eba61186a30a67c2762f44bd5283d590d4a020e
adride00 <adride@outlook.com>
2023-04-07 07:11:51 -0600

    Se limpio el archivo usePokemon

--- src/hooks/usePokemon.js
+++ src/hooks/usePokemon.js
@@ -1,25 +1,8 @@
-import { useEffect, useContext } from 'react'
+import { useContext } from 'react'
 import { PokeContext } from '../context/pokeContext.jsx'
 
 export default function usePokemon () {
   const { pokemons, setPokemons } = useContext(PokeContext)
-  useEffect(() => {
-    // if (!pokemons) return
-
-    // if (data) {
-    //   const { results } = data
-    //   console.log(data, loading, error)
-    //   setPokemons(results)
-    // }
-
-    // fetch('https://pokeapi.co/api/v2/pokemon')
-    //   .then(res => res.json())
-    //   .then(data => {
-    //     const { results } = data
-
-    //     setPokemons(results)
-    //   })
-  }, [])
 
   return [pokemons, setPokemons]
 }
