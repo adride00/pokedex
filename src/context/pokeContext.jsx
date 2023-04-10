60592fa4e68e810c36c23296d201bc9a858603ba
adride00 <adride@outlook.com>
2023-04-09 21:10:35 -0600

    se agrego paginacion

--- src/context/pokeContext.jsx
+++ src/context/pokeContext.jsx
@@ -5,13 +5,19 @@ export const PokeContext = createContext()
 export function PokemonProvider ({ children }) {
   const [pokemons, setPokemons] = useState([])
   const [errorSearch, setErrorSearch] = useState(false)
+  const [previusPage, setPreviusPage] = useState('')
+  const [nextPage, setNextPage] = useState('')
 
   return (
     <PokeContext.Provider value={{
       pokemons,
       setPokemons,
       errorSearch,
-      setErrorSearch
+      setErrorSearch,
+      previusPage,
+      setPreviusPage,
+      nextPage,
+      setNextPage
     }}
     >
       {children}
